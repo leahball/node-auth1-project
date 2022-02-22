90ce644f72e2102f2b488f78fa4eed65f9c5f5d3
iamleahb <leah.ball@gmail.com>
2022-02-21 18:37:36 -0600

    express server est, middleware scaffolded out

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const usersRouter = require('./users/users-router')
+const authRouter = require('./auth/auth-router')
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -11,7 +13,7 @@ const cors = require("cors");
   Users that do authenticate should have a session persisted on the server,
   and a cookie set on the client. The name of the cookie should be "chocolatechip".
 
-  The session can be persisted in memory (would not be adecuate for production)
+  The session can be persisted in memory (would not be adequate for production)
   or you can use a session store like `connect-session-knex`.
  */
 
@@ -20,6 +22,10 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/users', usersRouter)
+server.use('/api/auth', authRouter)
+
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
